c12607e886b1950483802746ff83bc9523d2254e
Mukul Gupta <mukul.gupta@gmail.com>
2017-03-28 13:20:25 +0000

    Removed log calls

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var requestHeader = {};
 app.enable('trust proxy');
 
 app.get('/api/whoami', function(req, res){
-    requestHeader['ipaddress'] = req.ip;
-    console.log(req.headers);
-    requestHeader['language'] = req.acceptsLanguages()[0];
     var userAgent = req.headers['user-agent'];
-    requestHeader['software'] = userAgent.match(/\((.*?)\)/)[0];
+    requestHeader['ipaddress'] = req.ip;
+    requestHeader['language'] = req.acceptsLanguages()[0];
+    requestHeader['software'] = userAgent.match(/\((.*?)\)/)[1];
     res.end(JSON.stringify(requestHeader));
 })
 
